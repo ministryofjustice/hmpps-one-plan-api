5c061869d25b9528855ccd3b6e56e8eb2a5174c8
Matthew Kerry <matthew.kerry@digital.justice.gov.uk>
2024-01-25 12:05:00 +0000

    not null stuff

--- src/main/resources/db/migration/postgres/V1_1__initial.sql
+++ src/main/resources/db/migration/postgres/V1_1__initial.sql
@@ -1,30 +1,30 @@
 create table plan
 (
     id            uuid primary key,
-    reference     uuid,
-    created_at    timestamp(3) with time zone,
-    created_by    varchar(50),
-    updated_at    timestamp(3) with time zone,
-    updated_by    varchar(50),
+    reference     uuid                        not null,
+    created_at    timestamp(3) with time zone not null,
+    created_by    varchar(50)                 not null,
+    updated_at    timestamp(3) with time zone not null,
+    updated_by    varchar(50)                 not null,
 
-    prison_number varchar(10),
-    type          smallint
+    prison_number varchar(10)                 not null,
+    type          smallint                    not null
 );
 
 create table objective
 (
     id                     uuid primary key,
-    reference              uuid,
-    created_at             timestamp(3) with time zone,
-    created_by             varchar(50),
-    updated_at             timestamp(3) with time zone,
-    updated_by             varchar(50),
+    reference              uuid                        not null,
+    created_at             timestamp(3) with time zone not null,
+    created_by             varchar(50)                 not null,
+    updated_at             timestamp(3) with time zone not null,
+    updated_by             varchar(50)                 not null,
 
-    title                  varchar(512),
-    target_completion_date date,
-    status                 varchar(50),
-    note                   text,
-    outcome                text
+    title                  varchar(512)                not null,
+    target_completion_date date                        not null,
+    status                 varchar(50)                 not null,
+    note                   text                        not null,
+    outcome                text                        not null
 );
 
 create table plan_objective_link
@@ -42,12 +42,12 @@ create table objective_history
     id                uuid primary key,
     objective_id      uuid,
 
-    previous_value    text,
-    new_value         text,
+    previous_value    text                        not null,
+    new_value         text                        not null,
 
-    updated_at        timestamp(3) with time zone,
-    updated_by        varchar(50),
-    reason_for_change varchar(250),
+    updated_at        timestamp(3) with time zone not null,
+    updated_by        varchar(50)                 not null,
+    reason_for_change varchar(250)                not null,
 
     constraint objective_id_fk foreign key (objective_id) references objective (id)
 );
@@ -55,17 +55,16 @@ create table objective_history
 create table step
 (
     id           uuid primary key,
-    objective_id uuid,
-    reference    uuid,
-    description  varchar(512),
-    step_order   smallint,
-    status       varchar(50),
+    objective_id uuid                        not null,
+    reference    uuid                        not null,
+    description  varchar(512)                not null,
+    step_order   smallint                    not null,
+    status       varchar(50)                 not null,
 
-    created_at   timestamp(3) with time zone,
-    created_by   varchar(50),
-    updated_at   timestamp(3) with time zone,
-    updated_by   varchar(50),
-
+    created_at   timestamp(3) with time zone not null,
+    created_by   varchar(50)                 not null,
+    updated_at   timestamp(3) with time zone not null,
+    updated_by   varchar(50)                 not null,
 
     constraint objective_id_fk foreign key (objective_id) references objective (id)
 );
@@ -73,12 +72,12 @@ create table step
 create table step_history
 (
     id                uuid primary key,
-    step_id           uuid,
-    previous_value    text,
-    new_value         text,
-    updated_at        timestamp(3) with time zone,
-    updated_by        varchar(50),
-    reason_for_change varchar(250),
+    step_id           uuid                        not null,
+    previous_value    text                        not null,
+    new_value         text                        not null,
+    updated_at        timestamp(3) with time zone not null,
+    updated_by        varchar(50)                 not null,
+    reason_for_change varchar(250)                not null,
 
 
     constraint objective_id_fk foreign key (step_id) references step (id)

6014d816bced0f65d7d9a5ea5fc557f7c545c9eb
Matthew Kerry <matthew.kerry@digital.justice.gov.uk>
2024-03-05 11:48:51 +0000

    Add auth info to api docs

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsoneplanapi/config/OpenApiConfig.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsoneplanapi/config/OpenApiConfig.kt
@@ -1,15 +1,67 @@
 package uk.gov.justice.digital.hmpps.hmppsoneplanapi.config
 
+import io.swagger.v3.oas.models.Components
 import io.swagger.v3.oas.models.OpenAPI
+import io.swagger.v3.oas.models.info.Contact
 import io.swagger.v3.oas.models.info.Info
+import io.swagger.v3.oas.models.security.OAuthFlow
+import io.swagger.v3.oas.models.security.OAuthFlows
+import io.swagger.v3.oas.models.security.Scopes
+import io.swagger.v3.oas.models.security.SecurityRequirement
+import io.swagger.v3.oas.models.security.SecurityScheme
+import org.springframework.boot.info.BuildProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @Configuration
-class OpenApiConfig {
+class OpenApiConfig(
+  buildProperties: BuildProperties,
+  apiUrlProperties: ApiUrlProperties,
+) {
+  private val version: String = buildProperties.version
+  private val oauthUrl = apiUrlProperties.oauth
+
   @Bean
   fun openApi(): OpenAPI = OpenAPI()
     .info(
-      Info().title("One Plan API"),
+      Info().title("One Plan API")
+        .version(version)
+        .description("API for viewing and managing Plans and Objectives for a person in prison or on probation")
+        .contact(Contact().name("HMPPS Digital Studio").email("feedback@digital.justice.gov.uk")),
     )
+    .components(
+      Components()
+        .addSecuritySchemes(
+          "bearer-jwt",
+          SecurityScheme()
+            .type(SecurityScheme.Type.HTTP)
+            .scheme("bearer")
+            .bearerFormat("JWT")
+            .`in`(SecurityScheme.In.HEADER)
+            .name("Authorization"),
+        ).addSecuritySchemes(
+          "hmpps-auth",
+          SecurityScheme()
+            .flows(getFlows())
+            .type(SecurityScheme.Type.OAUTH2)
+            .openIdConnectUrl("$oauthUrl/.well-known/openid-configuration"),
+        ),
+    )
+    .addSecurityItem(SecurityRequirement().addList("bearer-jwt", listOf("read", "write")))
+    .addSecurityItem(SecurityRequirement().addList("hmpps-auth"))
+
+  fun getFlows(): OAuthFlows {
+    val flows = OAuthFlows()
+    val clientCredflow = OAuthFlow()
+    clientCredflow.tokenUrl = "$oauthUrl/oauth/token"
+    val scopes = Scopes()
+      .addString("read", "Allows read of data")
+      .addString("write", "Allows write of data")
+    clientCredflow.scopes = scopes
+    val authFlow = OAuthFlow()
+    authFlow.authorizationUrl = "$oauthUrl/oauth/authorize"
+    authFlow.tokenUrl = "$oauthUrl/oauth/token"
+    authFlow.scopes = scopes
+    return flows.clientCredentials(clientCredflow).authorizationCode(authFlow)
+  }
 }

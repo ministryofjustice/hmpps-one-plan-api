513c388cb4a8627a20bff6a028f9ff1644d0aac7
ma226860 <142583965+ma226860@users.noreply.github.com>
2024-11-04 09:57:21 +0000

    HAAR-2582 Decommission hmpps-audit-sdk and use hmpps-sqs-spring-boot-starter instead (#43)

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,8 +2,8 @@ import org.springframework.boot.gradle.tasks.run.BootRun
 
 
 plugins {
-  val kotlinVersion = "2.0.20"
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "6.0.4"
+  val kotlinVersion = "2.0.21"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "6.0.8"
   kotlin("plugin.spring") version kotlinVersion
   kotlin("plugin.jpa") version kotlinVersion
 }
@@ -24,23 +24,23 @@ dependencies {
   implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
   implementation("io.github.oshai:kotlin-logging-jvm:7.0.0")
   implementation("org.springdoc:springdoc-openapi-starter-webflux-ui:2.6.0")
-  implementation("uk.gov.justice.service.hmpps:hmpps-audit-sdk:1.0.0")
+  implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:5.1.0")
   implementation("org.jsoup:jsoup:1.18.1")
 
-  runtimeOnly("org.flywaydb:flyway-core:10.17.3")
-  runtimeOnly("org.flywaydb:flyway-database-postgresql:10.17.3")
+  runtimeOnly("org.flywaydb:flyway-core:10.20.1")
+  runtimeOnly("org.flywaydb:flyway-database-postgresql:10.20.1")
   runtimeOnly("org.springframework.boot:spring-boot-starter-jdbc")
   runtimeOnly("org.postgresql:postgresql:42.7.4")
   runtimeOnly("org.postgresql:r2dbc-postgresql")
 
   testImplementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:4.3.2")
-  testImplementation(platform("org.testcontainers:testcontainers-bom:1.20.1"))
+  testImplementation(platform("org.testcontainers:testcontainers-bom:1.20.3"))
   testImplementation("org.testcontainers:postgresql")
   testImplementation("org.testcontainers:localstack")
   testImplementation("org.assertj:assertj-core:3.26.3")
   testImplementation("io.jsonwebtoken:jjwt-impl:0.12.6")
-  testImplementation("io.jsonwebtoken:jjwt-jackson:0.12.5")
-  testImplementation("io.mockk:mockk:1.13.12")
+  testImplementation("io.jsonwebtoken:jjwt-jackson:0.12.6")
+  testImplementation("io.mockk:mockk:1.13.13")
   testImplementation("com.ninja-squad:springmockk:4.0.2")
   testImplementation("org.springframework.security:spring-security-test")
   testImplementation("org.awaitility:awaitility:4.2.2")
